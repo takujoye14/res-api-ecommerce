955400eafdd8d32cbb4ded0ce454cc3427e4efaf
takujoye14 <takuma.joye414@gmail.com>
2025-03-17 10:39:49 +0100

    added get method (recently forgot)

--- src/index.js
+++ src/index.js
@@ -41,7 +41,9 @@ app.use(express.json());
 // ROUTES
 app.use("/api/users", userRoutes);
 
-
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
