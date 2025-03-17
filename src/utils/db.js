da8484be00b82d6fad830cbe02f20e96a88682ab
takujoye14 <takuma.joye414@gmail.com>
2025-03-17 16:27:19 +0100

    added dotenv

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
-const mongoConnection = "mongodb://atlas-sql-67c72eaf50cc3f3bf930205a-3ijkl.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin"
+dotenv.config()
+
+const uri = process.env.DB_CONNECTION
 
 mongoose.set("strictQuery", true)
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoConnection)
+    await mongoose.connect(uri)
     console.log("Connected to MongoDB")
   } catch (error) {
     console.error("No DB connection!", error)
-    process.exit(1) // Exit process with failure
+    process.exit(1) 
   }
 }
 
