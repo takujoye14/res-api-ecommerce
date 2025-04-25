5df4c450136b31198e9deb81461733afe8b64f1a
takujoye14 <takuma.joye414@gmail.com>
2025-04-25 19:07:23 +0200

    fixing tweaks

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const userRoutes = require("./routes/users");
-const connectDB = require("./utils/db");
-const path = require("path")
-const productRoutes = require("./routes/products");
-const invoicesRoutes = require("./routes/invoices");
-app.use(express.json());
+const path = require("path");
 
 const cors = require("cors");
+const connectDB = require("./utils/db");
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const invoicesRoutes = require("./routes/invoices");
 
 app.use(cors({
-    origin: ['https://solesupply.netlify.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }))
-  
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+  origin: [
+    'https://solesupply.netlify.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
+}))
 
-connectDB();
+app.use(express.json())
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+connectDB()
+
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/invoices", invoicesRoutes);
 
-
-
-
-
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
-
 app.get("/", (req, res) => {
-    res.send("Home page")
+  res.send("Home page");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Backend running on port ${port}`);
 });
 
-
+module.exports = app
