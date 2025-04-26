5b883eea7bb9c0d26e674c8cea70f7d4fac4bd29
takujoye14 <takuma.joye414@gmail.com>
2025-04-26 14:18:01 +0200

    again cors

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require("path");
-
-const connectDB = require("./utils/db");
 const userRoutes = require("./routes/users");
+const connectDB = require("./utils/db");
+const path = require("path");
 const productRoutes = require("./routes/products");
 const invoicesRoutes = require("./routes/invoices");
-
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://the-vaultexclusive.netlify.app/'], 
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  origin: ['https://the-vaultexclusive.netlify.app'],  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(express.json())
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-
-connectDB()
+connectDB();
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/invoices", invoicesRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Home page");
+  res.send("Backend is running!");
 });
 
 app.listen(port, () => {
-  console.log(`Backend running on port ${port}`);
+  console.log(`Backend listening on port ${port}`);
 });
 
-module.exports = app
+module.exports = app;
