3b41eb946bcff2ce4be562486ca86b75d29bce46
takujoye14 <takuma.joye414@gmail.com>
2025-04-28 14:05:55 +0200

    edited auth.js 5

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,13 +10,13 @@ router.post("/login", userLogin);
 
 router.post("/signup", hashPassword, userSignUp);
 
-router.put("/userUpdate", verifyToken, upload.single("image"), sharpMiddleware, (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "File upload error." });
-  }
+// router.put("/userUpdate", verifyToken, upload.single("image"), sharpMiddleware, (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: "File upload error." });
+//   }
 
-  const fileUrl = req.protocol + "://" + req.get("host") + "/" + req.file.processedPath;
-  res.json({ message: "User updated", fileUrl });
-});
+//   const fileUrl = req.protocol + "://" + req.get("host") + "/" + req.file.processedPath;
+//   res.json({ message: "User updated", fileUrl });
+// });
 
 module.exports = router;
